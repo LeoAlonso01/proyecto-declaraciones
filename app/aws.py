7c05b8561261e5b9c414ca8c9f60f7d4cfe20ac2
LeoAlonso01 <127887838+LeoAlonso01@users.noreply.github.com>
2025-01-16 14:48:10 -0600

    AWS keys secret

--- app/aws.py
+++ app/aws.py
@@ -1,11 +1,16 @@
 import boto3
 from botocore.exceptions import ClientError
+import os
+
+# credenciales de AWS desde el archivo .env
+AWS_SECRET_ID_KEY = os.getenv("AWS_SECRET_ID_KEY")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 
 # credenciales de AWS
 s3 = boto3.client(
     's3',
-    aws_access_key_id='AKIAU72LF6GUUEM6RWDH',
-    aws_secret_access_key='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',
+    aws_access_key_id=AWS_SECRET_ID_KEY,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
     region_name='us-east-1'
     )
 
