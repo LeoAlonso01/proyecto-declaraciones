2aa09ec09446824dfe9793ed22e027d1de1ce98c
LeoAlonso01 <127887838+LeoAlonso01@users.noreply.github.com>
2025-01-28 14:49:54 -0600

    regreso a la bd local

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from  app.routers import declaraciones, usuarios, nombramientos
 from app.database import create_tables
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Declaraciones API",
     description="API para el manejo de declaraciones patrimoniales",
     version="0.3",
 )
+
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 # creacion de tablas para la aplicacion
 @app.on_event("startup")
